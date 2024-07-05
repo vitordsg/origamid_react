5368c61de1402f39366da70e06ddc9a7b4f786d5
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-07-05 19:54:54 -0300

    :books: Arrow Function, Destructuring, Rest e Spread, Modules, Fetch, Async / Await, Arrays/Map e Filter e Expressões

--- 01-javascript/script3.js
+++ 01-javascript/script3.js
@@ -1,9 +1,11 @@
+// Fetch
 // fetch("https://ranekapi.origamid.dev/json/api/produto")
 //   .then((response) => response.json())
 //   .then((json) => {
 //     console.log(json);
 //   });
 
+// Async & Await
 async function fetchProdutos(url) {
   const response = await fetch(url);
   const json = await response.json();
@@ -15,3 +17,36 @@ const produtos = fetchProdutos(
 );
 
 console.log(produtos);
+
+// Arrays (Map e Filter)
+const precos = [
+  "Crédito",
+  "R$ 200",
+  "R$ 400",
+  "Contas Pagar",
+  "R$ 300",
+  "R$ 400",
+  "Meus Dados",
+];
+
+const precosFiltro = precos.filter((p) => p.includes("R$"));
+const precosNumeros = precosFiltro.map((preco) =>
+  Number(preco.replace("R$ ", ""))
+);
+
+console.log(precosFiltro);
+console.log(precosNumeros);
+console.log(precos);
+
+// Expressões
+const grupoA = 100;
+const grupoB = 200;
+
+if (grupoA > grupoB) {
+  console.log("Grupo A ganhou");
+} else {
+  console.log("Grupo B, ganhou");
+}
+
+const vencedor = grupoA > grupoB ? "Grupo A Venceu" : "Grupo B Venceu";
+console.log(vencedor)
