9afacd29eed6630004c24b66df102d9a72cdf868
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-07-17 22:18:23 -0300

    :books: JSX Exercicio

---
+++ 02-react-para-iniciantes/02-07-jsx-exercicio/src/App.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-constant-condition */
+import React from "react";
+
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+function App() {
+  const dados = mario;
+
+  const total = dados.compras
+    .map((item) => +item.preco.replace("R$ ", ""))
+    .reduce((a, b) => a + b);
+  console.log(total);
+
+  return (
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{" "}
+        <span style={{ color: dados.ativa ? "green" : "red" }}>
+          {dados.ativa ? "Ativo" : "Inativo"}
+        </span>
+      </p>
+      <p>Total: R$ {total}</p>
+      {total > 10000 && <p>Você está gastando muito!</p>}
+    </>
+  );
+}
+
+export default App;
