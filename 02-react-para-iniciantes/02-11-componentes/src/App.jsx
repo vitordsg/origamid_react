ca88eee063cfa030d5b53218b79adaaf58c6770e
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-07-31 21:43:12 -0300

    :books: Componentes React

---
+++ 02-react-para-iniciantes/02-11-componentes/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { act } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Form from "./Form/Form";
+
+const Teste = () => {
+  const active = false;
+  if (active) {
+    return <p>Teste</p>;
+  } else {
+    return null;
+  }
+};
+
+const App = () => {
+  return (
+    <>
+      <Teste />
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
