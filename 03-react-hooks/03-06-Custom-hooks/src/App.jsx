6de112ad607880f2fe724eacbb92c11358faeada
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-08-16 21:28:08 -0300

    :books: React Custom Hooks

---
+++ 03-react-hooks/03-06-Custom-hooks/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const App = () => {
+  const [produto, setProduto] = useLocalStorage("produto", "");
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  return (
+    <div>
+      <p>Produto preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </div>
+  );
+};
+export default App;
