1ea0fd077bbbca2e75ac934349dbae5b2f29c32b
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-08-23 22:07:11 -0300

    :books: React formulario Checkboxs

---
+++ 04-formularios/04-05-checkbox/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const App = () => {
+  const [termos, setTermos] = React.useState(false);
+  const [cores, setCores] = React.useState(["azul", "vermelho"]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  return (
+    <form>
+      <label>
+        <input
+          type="checkbox"
+          value="azul"
+          checked={cores.includes("azul")}
+          onChange={handleChange}
+        />
+        Azul
+      </label>
+
+      <label>
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={cores.includes("vermelho")}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
+    </form>
+  );
+};
+
+export default App;
