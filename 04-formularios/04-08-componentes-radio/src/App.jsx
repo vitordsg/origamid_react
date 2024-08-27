3cf0462396fa21313b32dd304e3d8d6fda7458c5
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-08-26 21:46:06 -0300

    :books: React formulario input radio

--- 04-formularios/04-08-componentes-radio/src/App.jsx
+++ 04-formularios/04-08-componentes-radio/src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Input from "./Form/input";
 import Select from "./Form/Select";
+import Radio from "./Form/Radio";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("Vermelho");
+  const [frutas, setFrutas] = React.useState("Vermelho");
+
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio options={["Limão", "Laranja", "Uva"]} value={frutas} setValue={setFrutas} />
+
       <Select
         options={["Smartphone", "Tablet"]}
         value={produto}
