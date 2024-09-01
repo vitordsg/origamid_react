cb1a8b6abcad051000af3331b506141f46a5ecf5
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-08-31 21:32:13 -0300

    :books: React CSS Import

---
+++ 05-css/05-01-css-import/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "./Title";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
