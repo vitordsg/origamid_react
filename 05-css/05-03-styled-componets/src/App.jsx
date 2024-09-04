ab34c22825a72243369c204d203fef972229b29a
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-09-03 23:57:34 -0300

    :books: React CSS Styled Components

--- 05-css/05-03-styled-componets/src/App.jsx
+++ 05-css/05-03-styled-componets/src/App.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return <div></div>;
-}
+const ProdutosContainer = styled.div`
+  display: flex;
+`;
+
+const Produto = styled.div`
+  flex: 1;
+`;
+
+const Titulo = styled.h1`
+  font-size: 3rem;
+  color: tomato;
+`;
+
+const Paragrafo = styled.p`
+  font-size: 1.5rem;
+  color: green;
+`;
+
+const Preco = styled.p`
+  background: hsl(${Math.random() * 360}, 100%, 50%);
+  color: white;
+`;
+
+const Comprar = styled.button`
+  background: #000;
+`;
+const App = () => {
+  return (
+    <div>
+      <ProdutosContainer>
+        <Produto>
+          <Titulo>Notebook</Titulo>
+          <Paragrafo>Meu texto é esse</Paragrafo>
+          <Preco cor="#84e">R$ 2000</Preco>
+        </Produto>
+
+        <Produto>
+          <Titulo>Smartphone</Titulo>
+          <Paragrafo>Meu texto é esse</Paragrafo>
+          <Preco cor="#53d956">R$ 1000</Preco>
+        </Produto>
+      </ProdutosContainer>
+    </div>
+  );
+};
 
 export default App;
