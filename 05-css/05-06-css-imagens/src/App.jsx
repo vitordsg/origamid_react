ca69179bfdcde1e88ade1ef7d009f357edaeb563
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-09-04 19:25:34 -0300

    :books: React CSS Imagens

---
+++ 05-css/05-06-css-imagens/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import foto from "./img/foto.jpg";
+import Dog from "./img/dog.svg?react";
+import DogSvg from "./DogSvg";
+
+const App = () => {
+  const [olho, setOlho] = React.useState(0);
+
+  function handleClick() {
+    for (let i = 0; i < 6; i++) {
+      setTimeout(() => {
+        setOlho(i);
+      }, 30 * i);
+    }
+  }
+
+  return (
+    <div>
+      <p className="fundo" onClick={handleClick}></p>
+      <Dog />
+      <DogSvg color="#84e" olho={olho} />
+      <img src={foto} alt="Cachorro" />
+    </div>
+  );
+};
+
+export default App;
