59bf27f780d2cb0fe274982d7efcebcc9fb9a2e0
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-09-11 17:29:20 -0300

    :books: React Router Head

---
+++ 06-react-router/06-05-head/src/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    console.log("Mudou a rota");
+  }, [location]);
+
+  return (
+    <nav>
+      <NavLink to="/" end>
+        Home
+      </NavLink>{" "}
+      <NavLink to="sobre">Sobre</NavLink>
+    </nav>
+  );
+};
+
+export default Header;
