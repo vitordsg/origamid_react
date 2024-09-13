f9f2cdcad63bfc68ecebe6ed16a7fc5aeb241422
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-09-13 20:17:25 -0300

    :books: React PropTypes

---
+++ 07-mais-react/07-01-proptypes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <>
+      <Button width={300}>
+        Clique Aqui
+      </Button>
+    </>
+  );
+};
+
+export default App;
